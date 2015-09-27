993100d540d32645154be69313e2c44a9442d1ce
theihor <the.ihor@gmail.com>
2015-09-27 13:13:24 +0300

    implemented f(m,k) matrix generation

--- lab1.py
+++ lab1.py
@@ -1,15 +1,16 @@
 import random
 import csv
 from os import urandom
+from math import ceil
 
 class Table:
-    def __init__(self, name = '', n = 0, m = 0):
+    def __init__(self, matrix, name = ''):
         self.name = name
-        self.content = [[0] * m] * n
-        self.row_names = ['row'] * n
-        self.col_names = ['col'] * m
-        self.n = n
-        self.m = m
+        self.content = matrix
+        self.n = len(matrix)
+        self.m = len(matrix[0])
+        self.row_names = ['K' + str(i + 1) for i in range(self.n)]
+        self.col_names = ['M' + str(i + 1) for i in range(self.m)]
 
     def fill_none(self):
         for i in range(self.n):
@@ -40,7 +41,7 @@ class Table:
 
 def rand(a = 0, b = 100):
     r = int.from_bytes(urandom(1), byteorder='little')
-    print('r =', r)
+    #print('r =', r)
     return a + round(r * (b - a) / 256.0)
 
 def random_pop(lst):
@@ -53,19 +54,6 @@ def random_pop(lst):
     x = lst[i]
     lst.remove(x)
     return x
-
-def gen_random_table(n, m, name = 'f(m,k)'):
-    t = Table(name, n, m)
-    c = t.content
-    for i in range(n):
-        available = list(range(m))
-        random.shuffle(available)
-        c[i] = [0] * m
-        for j in range(m):
-            print(t.content)
-            c[i][j] = random_pop(available)
-    t.content = c
-    return t
 
 def all_permutations(lst):
     if len(lst) == 1: return [lst]
@@ -84,7 +72,7 @@ def collide(lst, row):
     return False
 
 # WARNING: BRUTE FORCE IS USED
-def latin_square(n, filter):
+def latin_square(n, filter = lambda x: True):
     r = [x + 1 for x in range(n)]
     all_p = all_permutations(r)
     res = []
@@ -101,5 +89,51 @@ def latin_rectangle(m, N):
         res += latin_square(m, lambda x: not x in res)
     return res
 
+def read_config(config_file_name):
+    with open(config_file_name, 'r', encoding = 'utf8') as csvfile:    
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL, lineterminator='\n')
+        header = next(reader)
+        print(header)
+        res = []
+        for row in reader:
+            res.append({'m' : int(row[0]), 'k' : int(row[1]), 'e' : int(row[2])})
+        return res
 
+# matrix and config
+def apply_config(matrix, conf):
+    for c in conf:
+        m = c['m'] - 1; k = c['k'] - 1; e = c['e']
+        row = next(x for x in matrix if x[m] == e)
+        i = matrix.index(row)
+        matrix[i] = matrix[k]
+        matrix[k] = row
+    return matrix
+
+def make_matrix1(m,k,e):
+    if m == k and m == e: 
+        return latin_square(m)
+    elif m < k and m == e:
+        res = latin_rectangle(m, ceil(k / m))
+        res = res[:k]
+    elif m < k and m < e:
+        res = latin_rectangle(e, ceil(k / e))
+        res = res[:k]
+        for row in res:
+            row = row[:m]
+    else:
+        print('Invalid m, k, e parameters')
+    return res
+   
+def prior_probs(n, r = True):
+    a = [1] * n
+    p = [1.0 / n] * n
+    if r:
+        s = 0
+        for i in range(n): 
+            a[i] = rand(1,256)
+            s += a[i]
+        for i in range(n): 
+            p[i] = a[i] / s
+    return (a, p)
+        
 
